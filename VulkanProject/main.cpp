4b7e69452566dc8784a5cf1a2cbbe39259e94ed2
blackscalare <blackscalare@gmail.com>
2023-05-15 23:29:57 +0200

    queue families

--- VulkanProject/main.cpp
+++ VulkanProject/main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <cstring>
 #include <map>
+#include <optional>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -44,6 +45,14 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT
 
 class HelloTriangleApplication {
 public:
+	struct QueueFamilyIndices {
+		std::optional<uint32_t> graphicsFamily;
+
+		bool isComplete() {
+			return graphicsFamily.has_value();
+		}
+	};
+
 	void run() {
 		initWindow();
 		initVulkan();
@@ -120,13 +129,44 @@ private:
 		score += deviceProperties.limits.maxImageDimension2D;
 
 		// Application can't function without geometry shaders
-		if (!deviceFeatures.geometryShader) {
+		if (!deviceFeatures.geometryShader || !isDeviceSuitable(device)) {
 			return 0;
 		}
 
 		std::cout << "Found device " << deviceProperties.deviceName << " with score " << score << std::endl;
 
 		return score;
+	}
+
+	bool isDeviceSuitable(VkPhysicalDevice device) {
+		QueueFamilyIndices indices = findQueueFamilies(device);
+
+		return indices.isComplete();
+	}
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+		QueueFamilyIndices indices;
+		
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+		int i = 0;
+		for (const auto& queueFamily : queueFamilies) {
+			if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+				indices.graphicsFamily = i;
+			}
+
+			if (indices.isComplete()) {
+				break;
+			}
+
+			i++;
+		}
+
+		return indices;
 	}
 
 	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
